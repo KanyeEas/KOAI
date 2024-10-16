a25608142486dab88beb3bd6a36afc6567d9c819
wille_ost <wille_ost@protonmail.com>
2024-10-16 22:53:56 +0800

    create KOAI_backend SpringBoot project

---
+++ KOAI_Backend/src/main/java/io/wille_ost/koai_backend/KoaiBackendApplication.java
@@ -0,0 +1,13 @@
+package io.wille_ost.koai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoaiBackendApplication.class, args);
+	}
+
+}
---
+++ KOAI_Backend/src/test/java/io/wille_ost/koai_backend/KoaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wille_ost.koai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

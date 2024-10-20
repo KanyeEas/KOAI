035c806830996a536bdcae97ad957eed19238b08
wille_ost <wille_ost@protonmail.com>
2024-10-20 17:29:30 +0800

    project struct build complete

--- KOAI_Backend/src/main/java/io/wille_ost/koai_backend/controller/pk/BotInfoController.java
+++ KOAI_Backend/src/main/java/io/wille_ost/koai_backend/controller/pk/BotInfoController.java
@@ -3,11 +3,17 @@ package io.wille_ost.koai_backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
-    @RequestMapping("index/")
-    public String index() {
-        return "Hello World!";
+    @RequestMapping("getbotinfo/")
+    public Map<String, String> index() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "Wille OST");
+        map.put("version", "1.0.0");
+        return map;
     }
 }
